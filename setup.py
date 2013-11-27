e2a30e91f3681bddb63f632db9a2234022381c8a
Raul Gutierrez S <rgs@itevenworks.net>
2013-11-27 10:44:35 -0800

    Bump version to 0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='zk_shell',
-      version='0.7',
+      version='0.8',
       description='A Python - Kazoo based - shell for ZooKeeper',
       long_description=readme(),
       classifiers=[
