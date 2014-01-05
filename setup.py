ddcccb7ae1a8dc32cd48710e8e7cc9467646dd00
Raul Gutierrez S <rgs@itevenworks.net>
2014-01-05 01:46:19 -0800

    Release version 0.92.5
    Signed-off-by: Raul Gutierrez S <rgs@itevenworks.net>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='zk_shell',
-      version='0.92.4',
+      version='0.92.5',
       description='A Python - Kazoo based - shell for ZooKeeper',
       long_description=readme(),
       classifiers=[
