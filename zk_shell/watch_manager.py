a8838e56867748f8c377804c123599827c5a3a42
Raul Gutierrez S <rgs@itevenworks.net>
2014-04-27 11:19:55 -0700

    Cleaned WatchManager a little (but it should use the watcher recipes)
    Signed-off-by: Raul Gutierrez S <rgs@itevenworks.net>

--- zk_shell/watch_manager.py
+++ zk_shell/watch_manager.py
@@ -19,24 +19,24 @@ class WatchManager(object):
     """ keep track of paths being watched """
     def __init__(self, client):
         self._client = client
-        self._watching_paths = {}
+        self._stats_by_path = {}
 
     PARENT_ERR = "%s is a parent of %s which is already watched"
     CHILD_ERR = "%s is a child of %s which is already watched"
 
     def add(self, path, debug):
-        if path in self._watching_paths:
+        if path in self._stats_by_path:
             print("%s is already being watched" % (path))
             return
 
         # we can't watch child paths of what's already being watched,
         # because that generates a race between firing and resetting
         # watches for overlapping paths.
-        if "/" in self._watching_paths:
+        if "/" in self._stats_by_path:
             print("/ is already being watched, so everything is watched")
             return
 
-        for epath in self._watching_paths:
+        for epath in self._stats_by_path:
             if epath.startswith(path):
                 print(self.PARENT_ERR % (path, epath))
                 return
@@ -45,38 +45,38 @@ class WatchManager(object):
                 print(self.CHILD_ERR % (path, epath))
                 return
 
-        self._watching_paths[path] = PathStats(debug)
-        self._set_watches(path)
+        self._stats_by_path[path] = PathStats(debug)
+        self._watch(path)
 
     def remove(self, path):
-        if path not in self._watching_paths:
+        if path not in self._stats_by_path:
             print("%s is not being watched" % (path))
-            return
-        del self._watching_paths[path]
+        else:
+            del self._stats_by_path[path]
 
     def stats(self, path):
-        if path not in self._watching_paths:
+        if path not in self._stats_by_path:
             print("%s is not being watched" % (path))
-            return
+        else:
+            print("\nWatches Stats\n")
+            for path, count in self._stats_by_path[path].paths.items():
+                print("%s: %d" % (path, count))
 
-        print("\nWatches Stats\n")
-        for path, count in self._watching_paths[path].paths.items():
-            print("%s: %d" % (path, count))
-
-    def _set_watches(self, path):
+    def _watch(self, path):
         """
         we need to catch ZNONODE because children might be removed whilst we
         are iterating (specially ephemeral znodes)
         """
         try:
-            children = self._client.get_children(path, self._watches_stats_watcher)
-            for child in children:
-                self._set_watches("%s/%s" % (path, child))
+            children = self._client.get_children(path, self._watcher)
         except NoNodeError:
-            pass
+            children = []
 
-    def _watches_stats_watcher(self, watched_event):
-        for path, stats in self._watching_paths.items():
+        for child in children:
+            self._watch("%s/%s" % (path, child))
+
+    def _watcher(self, watched_event):
+        for path, stats in self._stats_by_path.items():
             if not watched_event.path.startswith(path):
                 continue
 
@@ -88,8 +88,8 @@ class WatchManager(object):
 
         if watched_event.type == EventType.CHILD:
             try:
-                self._client.get_children(watched_event.path,
-                                          self._watches_stats_watcher)
+                children = self._client.get_children(watched_event.path,
+                                                     self._watcher)
             except NoNodeError:
                 pass
 
