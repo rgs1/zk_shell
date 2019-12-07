2358cc3c7d76dc9aa849f715385064ef5ef3e527
Raúl Gutiérrez Segalés <rgs@itevenworks.net>
2019-12-07 12:28:56 -0500

    Fix build (#108)
    * Fix build
    
    Also, use https to fetch zookeeper's tarball.
    
    Signed-off-by: Raul Gutierrez Segales <rgs@pinterest.com>

--- ensure-zookeeper-env.sh
+++ ensure-zookeeper-env.sh
@@ -6,16 +6,15 @@
 set -e
 
 HERE=`pwd`
-ZOO_BASE_DIR="$HERE/zookeeper"
+ZOO_BASE_DIR="${HERE}/zookeeper"
 ZOOKEEPER_VERSION=${ZOOKEEPER_VERSION:-3.5.4-beta}
-ZOOKEEPER_PATH="$ZOO_BASE_DIR/$ZOOKEEPER_VERSION"
-ZOO_MIRROR_URL="http://apache.osuosl.org/"
-
+ZOOKEEPER_PATH="${ZOO_BASE_DIR}/${ZOOKEEPER_VERSION}"
+ZOO_MIRROR_URL="https://archive.apache.org"
 
 function download_zookeeper(){
     mkdir -p $ZOO_BASE_DIR
     cd $ZOO_BASE_DIR
-    curl --silent -C - $ZOO_MIRROR_URL/zookeeper/zookeeper-$ZOOKEEPER_VERSION/zookeeper-$ZOOKEEPER_VERSION.tar.gz | tar -zx
+    curl --silent -C - $ZOO_MIRROR_URL/dist/zookeeper/zookeeper-$ZOOKEEPER_VERSION/zookeeper-$ZOOKEEPER_VERSION.tar.gz | tar -zx
     mv zookeeper-$ZOOKEEPER_VERSION $ZOOKEEPER_VERSION
     chmod a+x $ZOOKEEPER_PATH/bin/zkServer.sh
 }
